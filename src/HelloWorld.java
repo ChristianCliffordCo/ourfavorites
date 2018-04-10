f601224013819e6e11ccf762c7ca6dc7ed1e7ab7
Christian <c_kennedy@coloradocollege.edu>
2018-04-10 11:19:20 -0600

    added new ish to helloworld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,14 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		
-		System.out.println("The world says Hello back");
-		
-		System.out.println("The world also says waddup");
-		
-		System.out.println("The world also says nothing");
-
-		
-		System.out.println("World says yo yoo");
+		System.out.println("World responds with WADDUP");
 	}
 
 }
